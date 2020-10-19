46d2fe52fda21270f02858650c5ecde0a98853d2
Lee-kraken <youzhaxiaoniqiu@gmail.com>
2020-10-19 08:52:13 +0800

    1-1 correction

--- assignment 1-1.cpp
+++ assignment 1-1.cpp
@@ -1,58 +1,71 @@
-#include <cstdio>
-#include <cstring>
-#include <vector>
-#include <string>
-#include <map>
-#include <algorithm>
-
-using namespace std;
-
 // 练习课1.1 
 /*
-    给定一个序列长度n，和序列a[n]，不断删除前两个值，且将前两个值的和
-    插入两数之前的位置，直至序列为回文序列，统计操作次数。
+给定一个序列长度n，和序列a[n]，删除任意两个相邻值，且将前两个值的和
+插入两数之前的位置，直至序列为回文序列，统计操作次数。
 
-    测试样例 0
-    2
-    1 1
-    测试样例 1
-    4
-    1 1 1 3
-    测试样例 2
-    5
-    1 3 4 4 8
+思想：	（1）当序列长度为1时，或当判断是回文序列时，比较最小次数并跳出；
+		（2）从序列给定位置开始循环删除并添加值，执行递归函数，再恢复之前的序列。
+
+测试样例 0
+2
+1 1
+测试输出 0
+min count: 1
+测试样例 1
+4
+1 1 1 3
+测试输出 1
+min count: 2
+测试样例 2
+5
+7 3 1 2 7
+测试输出 2
+min count: 1
 */
-int n, res = 0;
-vector<int> num;
+int n, minCount = INT32_MAX;
+vector<int> num, countList;		// num 记录输入序列，countList记录是回文序列时的操作次数
+vector<vector<int> > vecList;	// vecList记录回文序列
 
 bool IsPalindrome(vector<int> a) {
-    if (a.size() == 1) return true;
-    for (unsigned int i = 0; i < a.size(); i++)
-        if (a[i] != a[a.size() - i - 1])
-            return false;
+	if (a.size() == 1) return true;
+	for (unsigned int i = 0; i < a.size(); i++)
+		if (a[i] != a[a.size() - i - 1])
+			return false;
 
-    return true;
+	return true;
+}
+
+void function(vector<int> &a, int fcount) {
+	if (a.size() == 1 || IsPalindrome(a)) {
+		countList.push_back(fcount);
+		vecList.push_back(a);
+		if (fcount < minCount)
+			minCount = fcount;
+		return;
+	}
+
+	for (int i = 0; i < a.size() - 1; i++)
+	{
+		int x = a[i], y = a[i+1];
+		a[i+1] = x + y;
+		a.erase(a.begin() + i);
+		function(a, fcount + 1);
+		a[i] = y;
+		a.insert(a.begin() + i, x);
+	}
 }
 
 int main() {
-    scanf("%d", &n);
-    int t;
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &t);
-        num.push_back(t);
-    }
-    // 合并前两个值到后一个，删除前一个
-    while (!IsPalindrome(num)) {
-        res++;
-        num[1] = num[0] + num[1];
-        num.erase(num.begin());
-    }
-
-    printf("Num lists after operation: ");
-    for each (int var in num)
-    {
-        printf(" %d", var);
-    }
-    printf("\n count: %d\n", res);
-    return 0;
+	scanf("%d", &n);
+	int t;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &t);
+		num.push_back(t);
+	}
+	function(num, 0);
+	printf("Num lists after operation: ");
+	for each (int var in num)
+		printf(" %d", var);
+	printf("\n min count: %d\n", minCount);
+	return 0;
 }
